2d6aef581ce0b7b2b73d1f25fceac003f0fa5c18
gagandhillon02 <gagan.dhillon045@gmail.com>
2022-01-31 11:34:01 -0500

    initial commit

---
+++ src/test/java/com/major/test8/Test8ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.major.test8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
